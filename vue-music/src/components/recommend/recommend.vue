af9ae0dbbdcbb3c5e5bccc65f4eb42145baa5856
chenleibing <biner@91this.com>
2017-10-31 15:36:55 +0800

    jsonp

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,11 +1,33 @@
 <template>
   <div class="recommend" ref="recommend">
-    热门推荐
+    <div class="recommend-content">
+      <div class="slider-wrapper"></div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import {getRecommend} from 'api/recommend'
+  import {ERR_OK} from 'api/config'
+
   export default {
+    created() {
+      this._getRecommend()
+    },
+    methods: {
+      _getRecommend() {
+        getRecommend().then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res)
+          }
+        })
+      }
+    }
   }
 </script>
 
