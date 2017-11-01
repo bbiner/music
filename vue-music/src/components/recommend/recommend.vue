dc0b2730a4e4bad1b7a90d5c0fccbd6451a05272
chenleibing <biner@91this.com>
2017-11-01 17:29:23 +0800

    banner

--- vue-music/src/components/recommend/recommend.vue
+++ vue-music/src/components/recommend/recommend.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="recommend" ref="recommend">
     <div class="recommend-content">
-      <div class="slider-wrapper"></div>
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl" alt="">
+            </a>
+          </div>
+        </slider>
+      </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
@@ -12,10 +20,16 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import Slider from 'base/slider/slider'
   import {getRecommend} from 'api/recommend'
   import {ERR_OK} from 'api/config'
 
   export default {
+    data() {
+      return {
+        recommends: []
+      }
+    },
     created() {
       this._getRecommend()
     },
@@ -23,10 +37,14 @@
       _getRecommend() {
         getRecommend().then((res) => {
           if (res.code === ERR_OK) {
+            this.recommends = res.data.slider
             console.log(res)
           }
         })
       }
+    },
+    components: {
+      Slider
     }
   }
 </script>
